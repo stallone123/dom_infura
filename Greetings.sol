ab19f46ed4709102602061727d0b034a8ba76c23
Stallone123 <high.risk@shaw.ca>
2018-04-23 22:57:45 -0700

    This is the first commit (second try)

---
+++ Greetings.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.11;
+
+contract Greetings {
+  string message;
+
+  function Greetings() public {
+    message = "Hello World!";
+  }
+
+  function setGreetings(string _message) public {
+    message = _message;
+  }
+  function getGreetings() public constant returns (string) {
+    return message;
+  }
+}
